aadf8c96ec6652f9804131311fe18e11b9c5e887
VanTho <ovantho094@gmail.com>
2025-05-11 18:09:01 +0700

    d

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.post('/login', async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // true in production (HTTPS)
+      secure: process.env.NODE_ENV === 'production', 
       sameSite: 'strict',
-      maxAge: 24 * 60 * 60 * 1000 // 1 day
+      maxAge: 24 * 60 * 60 * 1000 
     });
 
     return res.status(200).json({
